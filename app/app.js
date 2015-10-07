a8ce063cf58244ee188c82af659c606a212a54e2
tanUIT <12520379@gm.uit.edu.vn>
2015-10-07 17:07:48 +0700

    create route

--- app/app.js
+++ app/app.js
@@ -23,16 +23,6 @@ angular.module('todolist', ['ngRoute'])
         });
     }
   ])
-  .controller('LoginController', function($scope) {
-    // $scope.flash = 'Hello world! I am Tweedy bird.';
-    // $scope.img = 'http://img2.wikia.nocookie.net/__cb20121120221843/scratchpad/images/thumb/5/54/Tweety.gif/180px-Tweety.gif';
-  })
-  .controller('RegisterController', function($scope) {
-    // $scope.flash = 'This is about page, hihi!';
-  })
   .controller('TasksController', function($scope) {
-    // $scope.flash = 'This is about page, hihi!';
-  })
-  .controller('ForgotController', function($scope) {
-    // $scope.flash = 'Contact with us at here!';
-  });
+    
+  });;
