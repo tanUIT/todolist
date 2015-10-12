defade022736c10a09038b9314d8df9baf1c15b2
tanUIT <12520379@gm.uit.edu.vn>
2015-10-12 10:09:44 +0700

    update new funtions

--- app/app.js
+++ app/app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 angular.module('todolist', ['ngRoute'])
   .config(['$routeProvider',
     function($routeProvider) {
@@ -14,15 +16,100 @@ angular.module('todolist', ['ngRoute'])
           templateUrl: 'app/templates/forgot.html',
           controller: 'ForgotController'
         })
-        .when('/tasks', {
+        .when('/nodes/:node_id', {
           templateUrl: 'app/templates/tasks.html',
           controller: 'TasksController'
+        })
+        .when('/nodes', {
+          templateUrl: 'app/templates/nodes.html',
+          controller: 'NodesController'
         })
         .otherwise({
           redirectTo: '/login'
         });
     }
   ])
-  .controller('TasksController', function($scope) {
-    
-  });;
+  .controller('LoginController', function($scope, $http, $location) {
+    $scope.login = function () {
+      fetch('https://rest-sifoni-anhnt0212.c9.io/api/v1/login', {
+          method: 'post',
+          body: JSON.stringify({
+            email: $scope.email,
+            password: $scope.password
+          })
+      }).then(function(response) {
+          return response.json();
+      }).then(function(data) {
+          if ('token' in data) {
+            window.localStorage.setItem('token', data.token);
+            $location.path("/nodes");
+          } else {
+            alert('Login fail !');
+          }
+      }).catch(function(err) {
+         alert('Login fail !');
+      });
+    }
+  })
+  .controller('NodesController', function($scope, $http) {
+    $scope.notes = [];
+
+    $scope.loadNodes = function () {
+      fetch('https://rest-sifoni-anhnt0212.c9.io/api/v1/me/note', {
+          method: 'get',
+          headers: {
+            'Authorization': 'Bearer ' + window.localStorage.getItem('token')
+          }
+      }).then(function(response) {
+          return response.json();
+      }).then(function(data) {
+          $scope.notes = data;
+          $scope.$apply();
+      }).catch(function(err) {
+         alert('Load notes fail!');
+      });
+    }
+
+    $scope.loadNodes();
+  })
+  .controller('TasksController', function($scope, $http, $routeParams) {
+    $scope.tasks = [];
+
+    $scope.showTasks = function () {
+      fetch('https://rest-sifoni-anhnt0212.c9.io/api/v1/me/note/' + $routeParams.node_id, {
+          method: 'get',
+          headers: {
+            'Authorization': 'Bearer ' + window.localStorage.getItem('token')
+          }
+      }).then(function(response) {
+          return response.json();
+      }).then(function(data) {
+          $scope.tasks = data;
+          $scope.$apply();
+      }).catch(function(err) {
+         alert('Load tasks fail!');
+      });
+    }
+    $scope.showTasks();
+  })
+  .controller('RegisterController', function($scope, $html, $location) {
+    fetch('https://rest-sifoni-anhnt0212.c9.io/api/v1/register', {
+          method: 'post',
+          body: JSON.stringify({
+            username: $scope.username,
+            email: $scope.email,
+            password: $scope.password
+          })
+      }).then(function(response) {
+          return response.json();
+      }).then(function(data) {
+          if ('token' in data) {
+            window.localStorage.setItem('token', data.token);
+            $location.path("/nodes");
+          } else {
+            alert('Register fail !');
+          }
+      }).catch(function(err) {
+         alert('Register fail !');
+      });
+  });
